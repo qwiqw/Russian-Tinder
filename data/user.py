1cbb733bc4d736ad956252593b918be11ac2f238
Иван <alfutovivan@gmail.com>
2025-04-27 21:46:39 +0300

    Add files via upload

--- data/user.py
+++ data/user.py
@@ -1,22 +1,22 @@
-import datetime
 import sqlalchemy
 import sqlalchemy.orm as orm
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-
 from .db_session import SqlAlchemyBase
 
 
 class User(SqlAlchemyBase, UserMixin):
-    def set_password(self, password):
-        self.hashed_password = generate_password_hash(password)
-
-    def check_password(self, password):
-        return self.hashed_password == password
     __tablename__ = 'users'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     age = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
-    profession = sqlalchemy.Column(sqlalchemy.String, nullable=False)
-    password = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     gender = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    additionally = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    image = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
